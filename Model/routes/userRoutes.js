87c7837ffb65503ffcabbaf5202e36574c61796b
Ariel Gluzman <ariel.gluzman@gmail.com>
2025-09-02 22:03:25 +0300

    Add User Authentication on Backend

--- Model/routes/userRoutes.js
+++ Model/routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../../controllers/userController');
 
 router.post('/', userController.createUser);
-router.get('/:username', userController.getUser);
+//router.get('/:username', userController.getUser);
 router.put('/:username', userController.updateUser);
 router.delete('/:username', userController.deleteUser);
-router.get('/authenticate', userController.authenticateUser); // New route for authentication
+router.post('/authenticate', userController.authenticateUser); // New route for authentication
 
 module.exports = router;
