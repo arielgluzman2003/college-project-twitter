22ae6f62b982205bcee4b2095c09faedb044b205
Ariel Gluzman <ariel.gluzman@gmail.com>
2025-09-02 22:37:14 +0300

    create user

--- Model/app.js
+++ Model/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const connectDB = require('./db');
+const cors = require('cors');
 
 app.use(express.json());
+
+// Allow CORS for 127.0.0.1:5500
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}));
 
 connectDB().then(() => {
     app.use('/api/users', userRoutes);
--- Model/routes/userRoutes.js
+++ Model/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/userController');
 
-router.post('/', userController.createUser);
+router.post('/create', userController.createUser);
 //router.get('/:username', userController.getUser);
 router.put('/:username', userController.updateUser);
 router.delete('/:username', userController.deleteUser);
